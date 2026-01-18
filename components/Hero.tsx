9c06d952123f2957db51926150be3629b6f159fc
Kilex79 <kilex79@gmail.com>
2026-01-18 19:56:17 +0100

    deleted / from image.png

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -8,7 +8,7 @@ const Hero = () => {
             {/* Background Image */}
             <div className="absolute inset-0 z-0">
                 <Image
-                    src="/image.jpg"
+                    src="image.png"
                     alt="Veterinaria abrazando a un perro feliz"
                     fill
                     className="object-cover object-center"
